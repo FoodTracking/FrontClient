17599eb647daab439a8220da3002090e73b6013e
Lexcai <axel.boudeau@ynov.com>
2023-10-19 09:28:56 +0200

    add : styles + logo

---
+++ styles/colors.js
@@ -0,0 +1,43 @@
+export const Palette = {
+  black: "#000000",
+  white: "#FFFFFF",
+  whiteTransparent: "rgba(255, 255, 255, 0.6)",
+
+  halfGrey: "#f2f2f2",
+  lightGrey: "#E9E9E9",
+  grey: "#A9A9A9",
+  mediumGrey: "#626262",
+  darkGrey: "#494952",
+
+  blueGrey: "#606EA6",
+
+  halfBlue: "#dff1f6",
+  lightBlue: "#78CFEB",
+  blue: "#78CFEB",
+  darkBlue: "#53B5D8",
+  blueTransparent: "rgba(120, 207, 235, 0.1)",
+
+  extraLightRed: "#FFFAFE",
+  lightRed: "#FFDADE",
+  halfRed: "#F28992",
+  darkRed: "#C6727A",
+
+  red: "#C6202C",
+
+  halfGreen: "#E6FFE2",
+  green: "#D1FF8F",
+  darkGreen: "#7AB229",
+
+  background: "#F9F9F9",
+};
+
+export const ConsolePalette = {
+  black: "\x1b[30m",
+  white: "\x1b[37m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+};
