43c41dafda7428730e855760fbadbfe3c73bbc0d
Lexcai <axel.boudeau@ynov.com>
2023-10-28 16:42:32 +0200

    add: Switch comp for login, helpers for responsive

--- Screen/Auth/Login.tsx
+++ Screen/Auth/Login.tsx
@@ -6,6 +6,11 @@ import BaseInput from "../../src/components/Input/BaseInput";
 import { Palette } from "../../styles/colors";
 import { RootStackParamList } from "../../src/components/navigation/AuthStack";
 import { StackNavigationProp } from "@react-navigation/stack";
+import { NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import PropTypes from "prop-types";
+import Switch from "../../src/components/Button/Switch";
 
 type OnboardingScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -14,15 +19,64 @@ type OnboardingScreenNavigationProp = StackNavigationProp<
 
 export default function LoginScreen({
   updateAccess,
+  jwt,
 }: {
   updateAccess: (access: boolean) => void;
+  jwt: string;
 }) {
   const navigation = useNavigation<OnboardingScreenNavigationProp>();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isSelected, setIsSelected] = useState(false);
+
+  const storeData = async (accessToken: string, refreshToken: string) => {
+    try {
+      await AsyncStorage.setItem("accessToken", accessToken);
+      await AsyncStorage.setItem("refreshToken", refreshToken);
+      console.log("LOG FROM LOGIN: TOKEN STORED");
+      axios.defaults.headers.common["Authorization"] = accessToken;
+
+      AsyncStorage.getItem("accessToken").then((value) => {
+        console.log("LOG FROM LOGIN: REFRESHTOKEN STORED", value);
+      });
+
+      AsyncStorage.getItem("refreshToken").then((value) => {
+        console.log("LOG FROM LOGIN: REFRESHTOKEN STORED", value);
+      });
+    } catch (error) {
+      console.error("Error storing data:", error);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "https://api.follow-food.alexandre-pezat.fr/auth/login",
+        {
+          email: email,
+          password: password,
+        }
+      );
+      updateAccess(true);
+      //
+      // // console.log("LOG FROM LOGIN ", JSON.stringify(response.data));
+      // console.log("LOG FROM LOGIN ", JSON.stringify(response.data.accessToken));
+      // console.log(
+      //   "LOG FROM LOGIN: RefreshToken ",
+      //   JSON.stringify(response.data.refreshToken)
+      // );
+      const accessToken = response.data.accessToken;
+      const refreshToken = response.data.refreshToken;
+
+      storeData(accessToken, refreshToken);
+    } catch (error) {
+      console.error("An error occurred during login:", error);
+      console.log("LOG FROM LOGIN ", JSON.stringify(error));
+    }
+  };
 
   const onPressLogin = async () => {
-    navigation.navigate("Register", { email: "", password: "" });
+    navigation.navigate("Register", { email: email, password: password });
   };
 
   return (
@@ -36,6 +90,7 @@ export default function LoginScreen({
           height: 200,
         }}
       />
+
       <Text
         style={{
           alignSelf: "center",
@@ -48,7 +103,6 @@ export default function LoginScreen({
       <View style={{ marginHorizontal: 50 }}>
         <BaseInput
           style={{
-            height: "10%",
             borderWidth: 1,
             borderColor: "black",
             borderRadius: 10,
@@ -57,47 +111,50 @@ export default function LoginScreen({
           }}
           placeholder="Adresse Mail"
           value={email}
-          onChange={(text: string) => setEmail(text)}
+          onChange={(event: NativeSyntheticEvent<TextInputChangeEventData>) =>
+            setEmail(event.nativeEvent.text)
+          }
         />
-      </View>
-      <View style={{ marginHorizontal: 50 }}>
-        <BaseInput
-          style={{
-            height: "5%",
-            borderWidth: 1,
-            borderColor: "black",
-            borderRadius: 10,
-            marginTop: 30,
-            marginHorizontal: 30,
+        <View style={{ marginHorizontal: 50 }}>
+          <BaseInput
+            style={{
+              borderWidth: 1,
+              borderColor: "black",
+              borderRadius: 10,
+              marginTop: 30,
+              marginHorizontal: 30,
+            }}
+            placeholder="Password"
+            value={password}
+            onChange={(event: NativeSyntheticEvent<TextInputChangeEventData>) =>
+              setPassword(event.nativeEvent.text)
+            }
+          />
+        </View>
+        <BaseButton
+          style={{ alignSelf: "center", marginTop: 20 }}
+          title="Se connecter"
+          onPress={() => {
+            alert("Connexion");
+            handleLogin();
           }}
-          placeholder="Password"
-          value={password}
-          onChange={(text: string) => setPassword(text)}
         />
+        <Pressable
+          style={{
+            alignSelf: "center",
+            marginTop: 20,
+            marginBottom: 30,
+          }}
+          onPress={() => {
+            onPressLogin();
+          }}
+        >
+          <Text>Pas encore inscrit ?</Text>
+          <Text style={{ color: "blue", textAlign: "center" }}>
+            Inscrivez-vous
+          </Text>
+        </Pressable>
       </View>
-      <BaseButton
-        style={{ alignSelf: "center", marginTop: 20 }}
-        title="Se connecter"
-        onPress={() => {
-          alert("Connexion");
-          updateAccess(true);
-        }}
-      />
-      <Pressable
-        style={{
-          alignSelf: "center",
-          marginTop: 20,
-          marginBottom: 30,
-        }}
-        onPress={() => {
-          onPressLogin;
-        }}
-      >
-        <Text>Pas encore inscrit ?</Text>
-        <Text style={{ color: "blue", textAlign: "center" }}>
-          Inscrivez-vous
-        </Text>
-      </Pressable>
     </View>
   );
 }
---
+++ src/components/Button/Switch.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Animated, TouchableWithoutFeedback, ViewStyle } from "react-native";
+import { Motion } from "@legendapp/motion";
+import { customResponsiveWidth } from "../../../styles/helpers/helpers";
+import { Palette } from "../../../styles/colors";
+
+type SwitchProps = {
+  selected?: boolean;
+  setSelected?: () => void;
+  containerStyle?: ViewStyle;
+};
+
+function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
+
+export default function Switch({
+  selected,
+  setSelected = () => {},
+  containerStyle = {},
+}: SwitchProps) {
+  const animation = useRef<Animated.Value>(new Animated.Value(0)).current;
+
+  const prevSelected = usePrevious<boolean | undefined>(selected || undefined);
+
+  useEffect(() => {
+    if (prevSelected !== selected) {
+      Animated.timing(animation, {
+        useNativeDriver: false,
+        toValue: selected ? 1 : 0,
+        duration: 250,
+      }).start();
+    }
+  }, [selected, prevSelected, animation]);
+
+  return (
+    <TouchableWithoutFeedback onPress={setSelected}>
+      <Motion.View
+        animate={{
+          backgroundColor: selected ? Palette.red : Palette.grey,
+        }}
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          width: customResponsiveWidth(14),
+          padding: 4,
+          borderRadius: 50,
+          ...containerStyle,
+        }}
+      >
+        <Motion.View
+          initial={{ y: 0 }}
+          animate={{ x: selected ? customResponsiveWidth(6) : 0, y: 0 }}
+          style={{
+            width: customResponsiveWidth(6),
+            height: customResponsiveWidth(6),
+            backgroundColor: Palette.white,
+            borderRadius: 50,
+          }}
+        />
+      </Motion.View>
+    </TouchableWithoutFeedback>
+  );
+}
