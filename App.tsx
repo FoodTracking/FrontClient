1d8f463597d5bbd277230ec3dad71fc5e20c8a1d
Alexandre Pezat <alexandre@wikodit.fr>
2023-11-17 13:22:49 +0100

    feat: disable log box

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@rneui/themed";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { LogBox } from "react-native";
 import FlashMessage from "react-native-flash-message";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -13,6 +14,9 @@ import Navigation from "./src/navigation/RootStackNavigator";
 import { theme } from "./src/utils/theme";
 
 export default function App() {
+  LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
+  LogBox.ignoreAllLogs(); //Ignore all log notifications
+
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
